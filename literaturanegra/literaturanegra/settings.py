36d8518f8a81aae7f756b737bf3ea1a6549e44d8
Roselma Mendes <roselma.mendes@gmail.com>
2017-08-05 17:28:19 -0300

    Adiciona docker-compose e usa postgres.

--- literaturanegra/literaturanegra/settings.py
+++ literaturanegra/literaturanegra/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'autoras'
 ]
 
 MIDDLEWARE = [
@@ -73,12 +74,18 @@ WSGI_APPLICATION = 'literaturanegra.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'literatura',
+        'USER': 'lit-user',
+        'PASSWORD': 'mysecretpassword',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
