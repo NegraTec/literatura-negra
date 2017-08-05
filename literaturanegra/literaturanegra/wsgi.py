e1bf3cfac4be0db24bb73f11e45d1f72840b3d3a
Roselma Mendes <roselma.mendes@gmail.com>
2017-08-05 19:03:50 -0300

    Deploy para o staging: atualiza wsgi.

--- literaturanegra/literaturanegra/wsgi.py
+++ literaturanegra/literaturanegra/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "literaturanegra.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "literaturanegra.literaturanegra.settings")
 
 application = get_wsgi_application()
